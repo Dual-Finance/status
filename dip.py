a12a5573b2d79f13f520378bd80e9117f76745c3
Britt Cyr <cyrbritt@gmail.com>
2022-09-06 17:18:34 -0400

    Update webdriver tests to do withdraw

--- dip.py
+++ dip.py
@@ -9,108 +9,108 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+def init_wallet(driver, phrase, password):
+    ''' Init wallet'''
+
+    # add wallet to chrome
+    time.sleep(1)
+    driver.switch_to.window(driver.window_handles[1])
+    try:
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+            (By.XPATH, "//button[contains(text(),'Use Secret Recovery Phrase')]")))
+    except TimeoutException:
+        driver.switch_to.window(driver.window_handles[0])
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+            (By.XPATH, "//button[contains(text(),'Use Secret Recovery Phrase')]")))
+
+    time.sleep(1)
+    driver.find_element(
+        By.XPATH, "//button[contains(text(),'Use Secret Recovery Phrase')]").click()
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+        (By.XPATH, "//textarea[@placeholder='Secret phrase']")))
+    driver.find_element(
+        By.XPATH, "//textarea[@placeholder='Secret phrase']").send_keys(phrase)
+    driver.find_element(
+        By.XPATH, "//button[@class='sc-bdfBQB bzlPNH']").click()
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+        (By.XPATH, "//input[@placeholder='Password']")))
+    driver.find_element(
+        By.XPATH, "//input[@placeholder='Password']").send_keys(password)
+    driver.find_element(
+        By.XPATH, "//input[@placeholder='Confirm Password']").send_keys(password)
+    driver.find_element(
+        By.XPATH, "//input[@type='checkbox']").click()
+    driver.find_element(
+        By.XPATH, "//button[@type='submit']").click()
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+        (By.XPATH, "//button[contains(text(),'Continue')]")))
+    continue_ = driver.find_element(
+        By.XPATH, "//button[contains(text(),'Continue')]")
+    driver.execute_script("arguments[0].click();", continue_)
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+        (By.XPATH, "//button[contains(text(),'Finish')]")))
+    finish = driver.find_element(
+        By.XPATH, "//button[contains(text(),'Finish')]")
+    driver.execute_script("arguments[0].click();", finish)
+    main_window = driver.window_handles[0]
+    driver.switch_to.window(main_window)
+
+    return main_window
+
+
+def select_wallet(driver, main_window):
+    ''' Init wallet'''
+
+    # Sleep for the modal transitions so the elements are clickable
+    time.sleep(2)
+
+    # Accept the disclaimer
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+        (By.XPATH, "//button[contains(text(), 'Accept')]")))
+    accept_disclaimer = driver.find_element(
+        By.XPATH, "//button[contains(text(), 'Accept')]")
+    accept_disclaimer.click()
+
+    time.sleep(2)
+
+    # Skip the tutorial
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+        (By.XPATH, "//button[contains(text(), 'Skip')]")))
+    skip = driver.find_element(
+        By.XPATH, "//button[contains(text(), 'Skip')]")
+    skip.click()
+
+    # Select Wallet
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+        (By.XPATH, "//button[span[contains(text(), 'Select Wallet')]]")))
+    select_wallet_button = driver.find_element(
+        By.XPATH, "//button[span[contains(text(), 'Select Wallet')]]")
+    select_wallet_button.click()
+
+    # Choose Phantom
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+        (By.XPATH, "//button[span[contains(text(), 'Phantom')]]")))
+    phantom = driver.find_element(
+        By.XPATH, "//button[span[contains(text(), 'Phantom')]]")
+    phantom.click()
+
+    original_window = driver.current_window_handle
+    WebDriverWait(driver, 60).until(EC.number_of_windows_to_be(2))
+    for window_handle in driver.window_handles:
+        if window_handle != original_window:
+            driver.switch_to.window(window_handle)
+            break
+
+    WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+        (By.XPATH, "//button[contains(text(),'Connect')]")))
+    popup_connect = driver.find_element(
+        By.XPATH, "//button[contains(text(),'Connect')]")
+    popup_connect.click()
+    driver.switch_to.window(main_window)
+
 
 def deposit(values):
     ''' Deposit into a DIP '''
-
-    def init_wallet():
-        ''' Init wallet'''
-
-        # add wallet to chrome
-        time.sleep(1)
-        driver.switch_to.window(driver.window_handles[1])
-        try:
-            WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-                (By.XPATH, "//button[contains(text(),'Use Secret Recovery Phrase')]")))
-        except TimeoutException:
-            driver.switch_to.window(driver.window_handles[0])
-            WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-                (By.XPATH, "//button[contains(text(),'Use Secret Recovery Phrase')]")))
-
-        time.sleep(1)
-        driver.find_element(
-            By.XPATH, "//button[contains(text(),'Use Secret Recovery Phrase')]").click()
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-            (By.XPATH, "//textarea[@placeholder='Secret phrase']")))
-        driver.find_element(
-            By.XPATH, "//textarea[@placeholder='Secret phrase']").send_keys(values[1])
-        driver.find_element(
-            By.XPATH, "//button[@class='sc-bdfBQB bzlPNH']").click()
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-            (By.XPATH, "//input[@placeholder='Password']")))
-        driver.find_element(
-            By.XPATH, "//input[@placeholder='Password']").send_keys(values[2])
-        driver.find_element(
-            By.XPATH, "//input[@placeholder='Confirm Password']").send_keys(values[2])
-        driver.find_element(
-            By.XPATH, "//input[@type='checkbox']").click()
-        driver.find_element(
-            By.XPATH, "//button[@type='submit']").click()
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-            (By.XPATH, "//button[contains(text(),'Continue')]")))
-        continue_ = driver.find_element(
-            By.XPATH, "//button[contains(text(),'Continue')]")
-        driver.execute_script("arguments[0].click();", continue_)
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-            (By.XPATH, "//button[contains(text(),'Finish')]")))
-        finish = driver.find_element(
-            By.XPATH, "//button[contains(text(),'Finish')]")
-        driver.execute_script("arguments[0].click();", finish)
-        main_window = driver.window_handles[0]
-        driver.switch_to.window(main_window)
-
-        return main_window
-
-    def select_wallet():
-        ''' Init wallet'''
-
-        # Sleep for the modal transitions so the elements are clickable
-        time.sleep(2)
-
-        # Accept the disclaimer
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-            (By.XPATH, "//button[contains(text(), 'Accept')]")))
-        accept_disclaimer = driver.find_element(
-            By.XPATH, "//button[contains(text(), 'Accept')]")
-        accept_disclaimer.click()
-
-        time.sleep(2)
-
-        # Skip the tutorial
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-            (By.XPATH, "//button[contains(text(), 'Skip')]")))
-        skip = driver.find_element(
-            By.XPATH, "//button[contains(text(), 'Skip')]")
-        skip.click()
-
-        # Select Wallet
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-            (By.XPATH, "//button[span[contains(text(), 'Select Wallet')]]")))
-        select_wallet_button = driver.find_element(
-            By.XPATH, "//button[span[contains(text(), 'Select Wallet')]]")
-        select_wallet_button.click()
-
-        # Choose Phantom
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-            (By.XPATH, "//button[span[contains(text(), 'Phantom')]]")))
-        phantom = driver.find_element(
-            By.XPATH, "//button[span[contains(text(), 'Phantom')]]")
-        phantom.click()
-
-        original_window = driver.current_window_handle
-        WebDriverWait(driver, 60).until(EC.number_of_windows_to_be(2))
-        for window_handle in driver.window_handles:
-            if window_handle != original_window:
-                driver.switch_to.window(window_handle)
-                break
-
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
-            (By.XPATH, "//button[contains(text(),'Connect')]")))
-        popup_connect = driver.find_element(
-            By.XPATH, "//button[contains(text(),'Connect')]")
-        popup_connect.click()
-        driver.switch_to.window(main_window)
-
     def select_dip(token="BTC"):
         if token:
             logging.info("Selecting DIP for %s", token)
@@ -171,10 +171,9 @@ def deposit(values):
         driver.switch_to.window(main_window)
 
         # Sleep to see the result
-        time.sleep(100)
+        time.sleep(60)
 
     options = Options()
-
     options.add_extension("Phantom.crx")
     options.add_argument("--disable-gpu")
     options.add_argument("--headless=chrome")
@@ -191,22 +190,88 @@ def deposit(values):
 
     try:
         logging.info('Initializing wallet')
-        main_window = init_wallet()
+        main_window = init_wallet(driver, values[1], values[2])
         logging.info('Selecting wallet')
-        select_wallet()
+        select_wallet(driver, main_window)
 
         logging.info('Selecting DIP')
         token = values[3] if len(values) >= 4 else None
         select_dip(token)
-    except TimeoutException as error:
+    except (TimeoutException, ElementClickInterceptedException) as error:
         logging.info('Error. Saving screenshot')
-        # TODO: also save the html
+
         driver.save_screenshot('screenshot.png')
-        print(error)
+        with open("failure.html", "w", encoding="utf-8") as source_file:
+            source_file.write(driver.page_source)
+        print(repr(error))
         # raise error
-    except ElementClickInterceptedException as error:
+
+
+def withdraw(values):
+    ''' Withdraw from the first vault '''
+    def do_withdraw():
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+            (By.XPATH, "//button[div[contains(text(), 'WITHDRAW')]]")))
+        withdraw_button = driver.find_element(
+            By.XPATH, "//button[div[contains(text(), 'WITHDRAW')]]")
+        logging.info("Clicking Withdraw")
+        withdraw_button.click()
+
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+            (By.XPATH, "//button[div[contains(text(), 'Withdraw')]]")))
+        withdraw_modal_button = driver.find_element(
+            By.XPATH, "//button[div[contains(text(), 'Withdraw')]]")
+        logging.info("Clicking Withdraw modal")
+        withdraw_modal_button.click()
+
+        original_window = driver.current_window_handle
+        WebDriverWait(driver, 60).until(EC.number_of_windows_to_be(2))
+        for window_handle in driver.window_handles:
+            if window_handle != original_window:
+                logging.info('Switching to approval window')
+                driver.switch_to.window(window_handle)
+                break
+
+        WebDriverWait(driver, 60).until(EC.presence_of_element_located(
+            (By.XPATH, "//button[contains(text(),'Approve')]")))
+        approve = driver.find_element(
+            By.XPATH, "//button[contains(text(),'Approve')]")
+        logging.info("Clicking approve")
+        approve.click()
+        driver.switch_to.window(main_window)
+
+        # Sleep to see the result
+        time.sleep(60)
+
+    options = Options()
+    options.add_extension("Phantom.crx")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--headless=chrome")
+
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    options.add_experimental_option("prefs", prefs)
+
+    driver = webdriver.Chrome(
+        executable_path=ChromeDriverManager().install(), options=options)
+    logging.info("Successfully found chrome driver")
+
+    driver.get(values[0])
+    driver.maximize_window()
+
+    try:
+        logging.info('Initializing wallet')
+        main_window = init_wallet(driver, values[1], values[2])
+        logging.info('Selecting wallet')
+        select_wallet(driver, main_window)
+
+        logging.info('Doing withdraw')
+        do_withdraw()
+    except (TimeoutException, ElementClickInterceptedException) as error:
         logging.info('Error. Saving screenshot')
-        # TODO: also save the html
         driver.save_screenshot('screenshot.png')
-        print(error)
+
+        with open("failure.html", "w", encoding="utf-8") as source_file:
+            source_file.write(driver.page_source)
+
+        print(repr(error))
         # raise error
