0ef85f70fda63ccee1e8f568b6416dca2a682924
Britt Cyr <cyrbritt@gmail.com>
2022-09-07 12:09:46 -0400

    Change link in status page and make token price not applicable

--- app/src/components/Logo/index.tsx
+++ app/src/components/Logo/index.tsx
@@ -4,7 +4,7 @@ import styles from './Logo.module.scss';
 
 export function Logo() {
   return (
-    <a className={styles.logo} href="/">
+    <a className={styles.logo} href="https://beta.dual.finance">
       <img src={dualFiLogo} alt="DUAL Finance" />
     </a>
   );
