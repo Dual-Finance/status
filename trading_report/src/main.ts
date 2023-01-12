22ca40a50024af5b97383d8b47b11157e739cade
Britt Cyr <cyrbritt@gmail.com>
2023-01-12 11:59:45 -0500

    Include the block time in cancellations

--- trading_report/src/main.ts
+++ trading_report/src/main.ts
@@ -8,29 +8,47 @@ async function main() {
     "https://floral-skilled-borough.solana-mainnet.discover.quiknode.pro/38cf24edefbebeb60eb7516eff40f076ac0823af/",
     'confirmed' as Commitment);
 
+  let allSigObjs = [];
   // ckc  open orders account for BONK
-  const sigObjs = await connection.getSignaturesForAddress(new PublicKey("361WP3Vtw2H4r3yjrbobZr4qJAGQdsnY5kdMsDdvc61n"), {limit: 20});
-  //console.log(sigObjs);
-  const signatures = sigObjs.map((sigObj) => sigObj.signature);
+  let sigObjs = await connection.getSignaturesForAddress(
+    new PublicKey("361WP3Vtw2H4r3yjrbobZr4qJAGQdsnY5kdMsDdvc61n"),
+  );
 
-  // TODO: Sleep on each iter and build up a list of transactions in the last 24 hours
-  /*
-  const transactions = await connection.getTransactions(signatures.slice(0, 10));
-  for (const transaction of transactions) {
-    console.log(transaction.transaction.message.instructions[0].programIdIndex);
+  const currentTime = (new Date().getTime() / 1_000);
+  const cutoffTime = currentTime - 24 * 60 * 60;
+  while (true) {
+    allSigObjs = allSigObjs.concat(sigObjs);
+    if (sigObjs[sigObjs.length - 1].blockTime < cutoffTime) {
+      break;
+    }
+    console.log('Fetching more signatures');
+    sigObjs = await connection.getSignaturesForAddress(
+      new PublicKey("361WP3Vtw2H4r3yjrbobZr4qJAGQdsnY5kdMsDdvc61n"),
+      {before: sigObjs[sigObjs.length - 1].signature}
+    );
+    await new Promise(r => setTimeout(r, 1_000));
   }
-  */
 
+  // Trim to all signatures in the last 24 hours.
+  const signatures = allSigObjs.filter((sigObj) => sigObj.blockTime > cutoffTime).map((sigObj) => sigObj.signature);
+  console.log('There are', signatures.length, 'signatures to process');
 
-
-
+  // Sleep on each iter to stay safely below the 25/sec RPC throttling limit.
+  for (let i = 0; i < signatures.length / 10; ++i) {
+    const transactions = await connection.getTransactions(signatures.slice(10 * i, 10 * (i + 1)));
+    for (const transaction of transactions) {
+      parseTransaction(transaction);
+    }
+    console.log('Parsed', i * 10, 'transactions');
+    await new Promise(r => setTimeout(r, 1_000));
+  }
 
   // CreateOrder: programIdIndex = 13
   //const transactions = await connection.getTransactions(["5imVwvUMeibFkUtvCMjdD6KDbkgQdxKE3r892zB45zH8KEYe1Vcoetns3zmndS1mBWAoJ2o6sJdnxapcBtEyVWKZ"]);
 
   // CancelOrder: programIdIndex = 7
   const transactions = await connection.getTransactions(["JJXphDics5pvUAYCNrsgojtMuaEDVqNXAtQXUiXdpJuQ9kW6xm1k6hMixMJDLE4KpRo8xkx7yHZsMPgM1xUapd8"]);
-  parseTransaction(transactions[0].transaction.message);
+  parseTransaction(transactions[0]);
 
   // SettleFunds: programIdIndex = 8
   //const transactions = await connection.getTransactions(["wCF9nh8w16pYnvZKe3acTHDJDUPSNdYoZiphMivAnZRa2ZNBpNqJHYezTKKBcpELWkhek5N4QxVVQqR8XAYKTED"]);
--- trading_report/src/parseTransaction.ts
+++ trading_report/src/parseTransaction.ts
@@ -1,13 +1,18 @@
-import { Message } from "@solana/web3.js";
+import { TransactionResponse } from "@solana/web3.js";
 import { u128 } from "@project-serum/borsh";
 const bs58 = require('bs58')
 
 // Parse the transaction and return a list of events
-export async function parseTransaction(message: Message) {
-
+export async function parseTransaction(transactionResponse: TransactionResponse) {
+    const message = transactionResponse.transaction.message;
     for (const instruction of message.instructions) {
-        if (instruction.programIdIndex == 7) {
+        if (instruction.programIdIndex === 7) {
             const payload = bs58.decode(instruction.data);
+
+            // This is for cancelling by client order id can be ignored
+            if (payload.length !== 25) {
+                continue;
+            }
 
             const buf = Buffer.alloc(25, payload);
 
@@ -15,11 +20,16 @@ export async function parseTransaction(message: Message) {
 
             // Use the way that order ids are generated to determine the order that was cancelled.
             // https://github.com/openbook-dex/program/blob/b6a5cf11c87b3ae4021cde5e2c0ff83d64814902/dex/src/state.rs#L908
-            console.log(limitPrice);
+            console.log('Cancel', limitPrice, transactionResponse.blockTime);
         }
 
-        if (instruction.programIdIndex == 13) {
+        if (instruction.programIdIndex === 13) {
             const payload = bs58.decode(instruction.data);
+
+            // Malformed transactions, need to investigate.
+            if (payload.length !== 51) {
+                continue;
+            }
 
             const buf = Buffer.alloc(51, payload);
 
@@ -32,9 +42,7 @@ export async function parseTransaction(message: Message) {
             const client_order_id = buf.readBigUInt64LE(41);
             const _limit = buf.readUInt16LE(49);
 
-            //console.log(side, limit_price, max_coin_qty);
-            //console.log(client_order_id);
-            console.log(instruction);
+            console.log('NewOrder', limit_price, side, max_coin_qty, transactionResponse.blockTime);
         }
     }
 }
