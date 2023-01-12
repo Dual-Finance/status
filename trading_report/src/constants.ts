021700d03d49fab54123747fde9d171d95b0ef9f
Britt Cyr <cyrbritt@gmail.com>
2023-01-12 15:43:15 -0500

    Cumulative stats

--- trading_report/src/constants.ts
+++ trading_report/src/constants.ts
@@ -1,4 +1,9 @@
+import { PublicKey } from "@solana/web3.js";
 
 export const STEP_SIZE = 10;
 export const NEW_ORDER_INSTRUCTION_ID = 13;
 export const CANCEL_INSTRUCTION_ID = 7;
+export const OPENBOOK_FORK_ID = new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX');
+export const OPENBOOK_BONK_MARKET_ID = new PublicKey('8PhnCfgqpgFM7ZJvttGdBVMXHuU4Q23ACxCvWkbs1M71');
+export const OPENBOOK_MNGO_MARKET_ID = new PublicKey('3NnxQvDcZXputNMxaxsGvqiKpqgPfSYXpNigZNFcknmD');
+export const TRADING_ACCOUNT = new PublicKey('CkcJx7Uwgxck5zm3DqUp2N1ikkkoPn2wA8zf7oS4tFSZ');
--- trading_report/src/main.ts
+++ trading_report/src/main.ts
@@ -1,39 +1,46 @@
-import { Commitment, Connection, PublicKey } from '@solana/web3.js';
+import { Commitment, Connection } from '@solana/web3.js';
+import { Market } from '@project-serum/serum';
 import { parseTransaction } from './parseTransaction';
 import fetch from 'node-fetch';
 import { TradeResponse } from './types';
-import { STEP_SIZE } from './constants';
+import { OPENBOOK_BONK_MARKET_ID, OPENBOOK_FORK_ID, OPENBOOK_MNGO_MARKET_ID, STEP_SIZE, TRADING_ACCOUNT } from './constants';
 
 async function main() {
   console.log('Analysis running', new Date().toUTCString());
 
   const connection = new Connection(
-    "https://floral-skilled-borough.solana-mainnet.discover.quiknode.pro/38cf24edefbebeb60eb7516eff40f076ac0823af/",
-    'confirmed' as Commitment);
+    'https://floral-skilled-borough.solana-mainnet.discover.quiknode.pro/38cf24edefbebeb60eb7516eff40f076ac0823af/',
+    'confirmed' as Commitment,
+  );
+  const market = await Market.load(
+    connection,
+    OPENBOOK_BONK_MARKET_ID,
+    {},
+    OPENBOOK_FORK_ID,
+  );
+  const openOrders = await market.findOpenOrdersAccountsForOwner(connection, TRADING_ACCOUNT);
+  const openOrdersAccount = openOrders[0].address;
 
   let allSigObjs = [];
-  // ckc  open orders account for BONK
-  let sigObjs = await connection.getSignaturesForAddress(
-    new PublicKey("361WP3Vtw2H4r3yjrbobZr4qJAGQdsnY5kdMsDdvc61n"),
-  );
+  let sigObjs = await connection.getSignaturesForAddress(openOrdersAccount);
 
   const currentTime = (new Date().getTime() / 1_000);
   const cutoffTime = currentTime - 24 * 60 * 60;
   while (true) {
+    await new Promise(r => setTimeout(r, 1_000));
     allSigObjs = allSigObjs.concat(sigObjs);
     if (sigObjs[sigObjs.length - 1].blockTime < cutoffTime) {
       break;
     }
     console.log('Fetching more signatures');
     sigObjs = await connection.getSignaturesForAddress(
-      new PublicKey("361WP3Vtw2H4r3yjrbobZr4qJAGQdsnY5kdMsDdvc61n"),
+      openOrdersAccount,
       {before: sigObjs[sigObjs.length - 1].signature}
     );
-    await new Promise(r => setTimeout(r, 1_000));
   }
 
   // Trim to all signatures in the last 24 hours.
-  const signatures = allSigObjs.filter((sigObj) => sigObj.blockTime > cutoffTime).map((sigObj) => sigObj.signature);
+  const signatures = allSigObjs.filter((sigObj) => sigObj.blockTime > cutoffTime).map((sigObj) => sigObj.signature).reverse();
   console.log('There are', signatures.length, 'signatures to process');
 
   // Sleep on each iter to stay safely below the 25/sec RPC throttling limit.
@@ -46,18 +53,24 @@ async function main() {
     await new Promise(r => setTimeout(r, 1_000));
   }
 
-  // CreateOrder: programIdIndex = 13
-  // const transactions = await connection.getTransactions(["5imVwvUMeibFkUtvCMjdD6KDbkgQdxKE3r892zB45zH8KEYe1Vcoetns3zmndS1mBWAoJ2o6sJdnxapcBtEyVWKZ"]);
-
-  // CancelOrder: programIdIndex = 7
-  // const transactions = await connection.getTransactions(["JJXphDics5pvUAYCNrsgojtMuaEDVqNXAtQXUiXdpJuQ9kW6xm1k6hMixMJDLE4KpRo8xkx7yHZsMPgM1xUapd8"]);
-
-  const url = 'https://mango-transaction-log.herokuapp.com/v4/stats/openbook-trades?address=361WP3Vtw2H4r3yjrbobZr4qJAGQdsnY5kdMsDdvc61n&address-type=open-orders&limit=10000'
+  const url = `https://mango-transaction-log.herokuapp.com/v4/stats/openbook-trades?address=${openOrdersAccount.toBase58()}&address-type=open-orders&limit=10000`
   const response = await fetch(url);
   const trades = await response.json() as TradeResponse[];
   const recentTrades = trades.filter((trade) => Date.parse(trade.block_datetime) / 1_000 > cutoffTime);
-  console.log(recentTrades);
-  console.log(recentTrades.length);
+
+  const sells = recentTrades.filter((trade) => trade.side === 'sell');
+  const buys = recentTrades.filter((trade) => trade.side === 'buy');
+
+  const totalSellsValue = sells.map((trade) => trade.size * trade.price).reduce(function(a, b) { return a + b; }, 0);
+  const totalBuysValue = buys.map((trade) => trade.size * trade.price).reduce(function(a, b) { return a + b; }, 0);
+
+  const totalSellsAmount = sells.map((trade) => trade.size).reduce(function(a, b) { return a + b; }, 0);
+  const totalBuysAmount = buys.map((trade) => trade.size).reduce(function(a, b) { return a + b; }, 0);
+
+  console.log(`Sold ${totalSellsAmount} for ${totalSellsValue} avg ${totalSellsValue / (totalSellsAmount + .0000000001)}`);
+  console.log(`Bought ${totalBuysAmount} for ${totalBuysValue} avg ${totalBuysValue / (totalBuysAmount + .0000000001)}`);
+
+  console.log('Analysis done', new Date().toUTCString());
 }
 
 main();
