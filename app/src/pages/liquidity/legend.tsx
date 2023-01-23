0c82bf28b4de128ef59f51dea7981f5431796348
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-01-23 17:44:03 -0400

    feat: add orders to legend, make icons circles

--- app/src/pages/liquidity/legend.tsx
+++ app/src/pages/liquidity/legend.tsx
@@ -5,13 +5,19 @@ export function Legend() {
   return (
     <Box display="flex" gap={3} justifyContent="center" width="100%">
       <Box display="flex" gap={1} alignItems="center">
-        <Square color="green" size="12px" />
+        <Line color="blue" width={4} />
+        <Typography variant="body1" color="textPrimary">
+          Orders
+        </Typography>
+      </Box>
+      <Box display="flex" gap={1} alignItems="center">
+        <Circle color="green" size="12px" />
         <Typography variant="body1" color="textPrimary">
           Buy
         </Typography>
       </Box>
       <Box display="flex" gap={1} alignItems="center">
-        <Square color="red" size="12px" />
+        <Circle color="red" size="12px" />
         <Typography variant="body1" color="textPrimary">
           Sell
         </Typography>
@@ -20,11 +26,20 @@ export function Legend() {
   );
 }
 
-type SquareProps = {
+type LineProps = {
+  color: string;
+  width: number;
+};
+
+function Line({ color, width }: LineProps) {
+  return <Box bgcolor={color} width={`${width * 3}px`} height={`${width}px`} />;
+}
+
+type CircleProps = {
   color: string;
   size: string;
 };
 
-function Square({ color, size }: SquareProps) {
-  return <Box bgcolor={color} width={size} height={size} />;
+function Circle({ color, size }: CircleProps) {
+  return <Box bgcolor={color} width={size} height={size} borderRadius={size} />;
 }
