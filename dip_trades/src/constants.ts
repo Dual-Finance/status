7ba32c8f47bb4ee2e23d8fbea2e0aea14ccb4f71
Britt Cyr <cyrbritt@gmail.com>
2023-01-28 01:31:13 -0500

    First commit for tracking MM sales (#88)

---
+++ dip_trades/src/constants.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const STEP_SIZE = 8;
+export const PREMIUM_ACCOUNT = new PublicKey('Cm8KXD1pVYSdH63RiWx3MZfZbTP6HR9MippEvpm6FGYn');
+export const MM_PREMIUM_ACCOUNT = new PublicKey('92RPCdacYHXu85MGcQRcSNZb2PstcZaGLDZh6rap5Gs2');
+export const OPTION_VAULT = new PublicKey('9SgZKdeTMaNuEZnhccK2crHxi1grXRmZKQCvNSKgVrCQ');
+export const USDC_MINT = new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+export const WSOL_MINT = new PublicKey('So11111111111111111111111111111111111111112');
