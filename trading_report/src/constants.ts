e8c5d3084fddb3eeeff284fb5ca3704adb64d0d0
DonDuala <107803907+DonDuala@users.noreply.github.com>
2023-03-05 10:02:45 -0400

    Dual report (#114)
    * Add DUAL to trading report
    
    * Increase trade-log size
    
    * fix workflow

--- trading_report/src/constants.ts
+++ trading_report/src/constants.ts
@@ -8,6 +8,7 @@ export const OPENBOOK_FORK_ID = new PublicKey('srmqPvymJeFKQ4zGQed1GFppgkRHL9kaE
 export const OPENBOOK_BONK_MARKET_ID = new PublicKey('8PhnCfgqpgFM7ZJvttGdBVMXHuU4Q23ACxCvWkbs1M71');
 export const OPENBOOK_MNGO_MARKET_ID = new PublicKey('3NnxQvDcZXputNMxaxsGvqiKpqgPfSYXpNigZNFcknmD');
 export const OPENBOOK_SOL_MARKET_ID = new PublicKey('8BnEgHoWFysVcuFFX7QztDmzuH8r5ZFvyP3sYwn1XTh6');
+export const OPENBOOK_DUAL_MARKET_ID = new PublicKey('H6rrYK3SUHF2eguZCyJxnSBMJqjXhUtuaki6PHiutvum');
 export const TRADING_ACCOUNT = new PublicKey('CkcJx7Uwgxck5zm3DqUp2N1ikkkoPn2wA8zf7oS4tFSZ');
 export const DIP_PROGRAM_ID = new PublicKey('DiPbvUUJkDhV9jFtQsDFnMEMRJyjW5iS6NMwoySiW8ki');
 export const SO_PROGRAM_ID = new PublicKey('4yx1NJ4Vqf2zT1oVLk4SySBhhDJXmXFt88ncm4gPxtL7');
@@ -16,4 +17,5 @@ export const SYMBOL_TO_OPENBOOK_MARKET_ID = {
     'MNGO': OPENBOOK_MNGO_MARKET_ID,
     'SOL': OPENBOOK_SOL_MARKET_ID,
     'BONK': OPENBOOK_BONK_MARKET_ID,
+    'DUAL': OPENBOOK_DUAL_MARKET_ID,
 }
