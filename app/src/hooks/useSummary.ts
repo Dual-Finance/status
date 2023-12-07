18ec29c11c55998c9373890a7cc213589925800e
diegofigs <12281609+diegofigs@users.noreply.github.com>
2023-12-07 12:21:27 -0400

    refactor: add initial token balance aggregation

--- app/src/hooks/useSummary.ts
+++ app/src/hooks/useSummary.ts
@@ -1,38 +1,43 @@
-import { useStakingOptions } from './useStakingOptions';
+import { StakingOptions } from '@dual-finance/staking-options';
+import { Connection } from '@solana/web3.js';
+import { useEffect, useState } from 'react';
+import { useAnchorProvider } from './useAnchorProvider';
+import { SoParams, useStakingOptions } from './useStakingOptions';
 
 export function useSummary(network: string) {
+  const [, connection] = useAnchorProvider(network);
   const soAccounts = useStakingOptions(network);
+  const [tvl, setTvl] = useState<number>();
 
-  if (!soAccounts) {
+  useEffect(() => {
+    if (!soAccounts) {
+      return;
+    }
+
+    fetchTvl(connection, soAccounts).then(setTvl).catch(console.error);
+  }, [connection, soAccounts]);
+
+  if (!soAccounts || !tvl) {
     return;
   }
 
-  const tokens = new Map();
-  const summary = soAccounts.reduce<SummaryRecords>(
+  const summary = soAccounts.reduce(
     (acc, account) => {
       const { baseMint, quoteMint } = account;
 
-      const baseAccounted = tokens.has(baseMint.toString()) ? 0 : 1;
-      if (baseAccounted) {
-        tokens.set(baseMint.toString(), baseMint);
-      }
-
-      const quoteAccounted = tokens.has(quoteMint.toString()) ? 0 : 1;
-      if (quoteAccounted) {
-        tokens.set(quoteMint.toString(), quoteMint);
-      }
+      acc.tokens.set(baseMint.toString(), baseMint);
+      acc.tokens.set(quoteMint.toString(), quoteMint);
 
       return {
         ...acc,
-        tokenCount: acc.tokenCount + baseAccounted + quoteAccounted,
         maxFees: acc.maxFees + (account.maxFees || 0),
         maxSettlement: acc.maxSettlement + (account.maxSettlement || 0),
       };
     },
-    { tvl: 0, tokenCount: 0, maxFees: 0, maxSettlement: 0 }
+    { tokens: new Map(), maxFees: 0, maxSettlement: 0 }
   );
 
-  return summary;
+  return { ...summary, tvl, tokenCount: summary.tokens.size };
 }
 
 export interface SummaryRecords {
@@ -40,4 +45,17 @@ export interface SummaryRecords {
   tokenCount: number;
   maxFees: number;
   maxSettlement: number;
+}
+
+async function fetchTvl(connection: Connection, soAccounts: SoParams[]) {
+  const tvl: { [mint: string]: number } = {};
+  const sdk = new StakingOptions(connection.rpcEndpoint);
+  // eslint-disable-next-line no-restricted-syntax
+  for (const account of soAccounts) {
+    const { baseMint } = account;
+    const baseVault = await sdk.baseVault(account.name, baseMint);
+    const balance = await connection.getTokenAccountBalance(baseVault);
+    tvl[baseMint.toString()] = (tvl[baseMint.toString()] || 0) + balance.value.uiAmount!;
+  }
+  return Object.entries(tvl).reduce((acc, [, value]) => acc + value, 0);
 }
