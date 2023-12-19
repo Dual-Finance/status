bb7e670a3d99d9f03cc391f914f8f39cad216f97
Britt Cyr <cyrbritt@gmail.com>
2023-12-19 10:24:28 -0500

    Comments

--- indexer/src/index.ts
+++ indexer/src/index.ts
@@ -1,32 +1,29 @@
 import postgres, { Sql } from "postgres";
 
-const {
-    DATABASE_URL,
-  } = process.env;
+const { DATABASE_URL } = process.env;
 
 async function indexAll() {
-    const sql = postgres(DATABASE_URL!, {
-      prepare: false,
-      idle_timeout: 3 * 60,
-      max_lifetime: 10 * 60,
-      types: {
-        bigint: postgres.BigInt,
-      },
-    });
+  const sql = postgres(DATABASE_URL!, {
+    prepare: false,
+    idle_timeout: 3 * 60,
+    max_lifetime: 10 * 60,
+    types: {
+      bigint: postgres.BigInt,
+    },
+  });
 
-    // Update the list of mints
-    // updateMints()
-  
-    const now = new Date().getTime();
-    const mints = await sql`SELECT address FROM so.mints WHERE expiration > now`;
-    // updateIssues()
-    // updateExercises()
-    // updateTransfers()
-  
-  }
-  
-  async function main() {
-    /*
+  // index all SO mints, and reverse mints
+  // indexMints()
+
+  const now = new Date().getTime();
+  const mints = await sql`SELECT address FROM so.mints WHERE expiration > now`;
+  // indexIssues()
+  // indexExercises()
+  // indexTransfers()
+}
+
+async function main() {
+  /*
     require("../../../ley.config.js");
     const ley = require("ley");
   
@@ -36,9 +33,9 @@ async function indexAll() {
     // run once full sync (last two days)
     await indexAll(2 * 24 * 60 * 60 * 2.5);
     */
-  
-    console.log("queueing incremental run every 600s");
-    // then run incremental every 10 minute
-    setInterval(() => indexAll(), 600 * 1000);
-  }
-  main();
+
+  console.log("queueing incremental run every 600s");
+  // then run incremental every 10 minute
+  setInterval(() => indexAll(), 600 * 1000);
+}
+main();
